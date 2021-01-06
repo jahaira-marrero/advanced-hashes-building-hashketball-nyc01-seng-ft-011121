7837c4acaf9ee4bcdafe254e6c80dd2ed1f624fc
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-06 16:26:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,7 @@ def game_hash
     :team_name => "Brooklyn Nets ",
     :colors => ["Black ", "White "],
     :players => [
-      {
-      :player_name => "Alan Anderson", 
+     {:player_name => "Alan Anderson", 
       :number => 0, 
       :shoe => 16, 
       :points => 22,
@@ -13,10 +12,8 @@ def game_hash
       :assists => 12,
       :steals => 3,
       :blocks => 1,
-      :slam_dunks => 1
-    },
-    {
-      :player_name => "Reggie Evans", 
+      :slam_dunks => 1},
+    {:player_name => "Reggie Evans", 
       :number => 30, 
       :shoe => 14, 
       :points => 12,
@@ -24,10 +21,8 @@ def game_hash
       :assists => 12,
       :steals => 12,
       :blocks => 12,
-      :slam_dunks => 7
-    },
-    {
-      :player_name => "Brook Lopez ", 
+      :slam_dunks => 7},
+     {:player_name => "Brook Lopez ", 
       :number => 11, 
       :shoe => 17, 
       :points => 17,
@@ -35,16 +30,8 @@ def game_hash
       :assists => 10,
       :steals => 3,
       :blocks => 1,
-      :slam_dunks => 15
-    }
-    ]
-  }, 
-  :away => {
-    :team_name => " ",
-    :colors => [" ", " "],
-    :players => [
-      {
-      :player_name => " ", 
+      :slam_dunks => 15},
+     {:player_name => " ", 
       :number => , 
       :shoe => , 
       :points => ,
@@ -52,8 +39,22 @@ def game_hash
       :assists => ,
       :steals => ,
       :blocks => ,
-      :slam_dunks => 
-    }
+      :slam_dunks => }
+    ]
+  }, 
+  :away => {
+    :team_name => " ",
+    :colors => [" ", " "],
+    :players => [
+     {:player_name => " ", 
+      :number => , 
+      :shoe => , 
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks => }
     ]
   }
   }
