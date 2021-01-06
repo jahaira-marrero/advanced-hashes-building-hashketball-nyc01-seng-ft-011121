66457cf5f9c4c4eae6e9184630d01eff4706e43e
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-06 15:00:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,27 +1,29 @@
 def game_hash
   teams = {
     :home => {
-    :team_name => " ",
-    :colors => [" ", " "],
-    :players => [{
-      :player_name => " ", 
-      :number => 1, 
-      :shoe => 9, 
-      :points => 50,
-      :rebounds => 10,
-      :assists => 5,
-      :steals => 5,
-      :blocks => 5,
-      :slam_dunks => 5
-    }]
+    :team_name => "Brooklyn Nets ",
+    :colors => ["Black ", "White "],
+    :players => [
+      {
+      :player_name => "Alan Anderson", 
+      :number => 0, 
+      :shoe => 16, 
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    },
+    ]
   }, 
   :away => {
     :team_name => " ",
     :colors => [" ", " "],
     :players => [{
       :player_name => " ", 
-      :number => 1, 
-      :shoe => 9, 
+      :number => , 
+      :shoe => , 
       :points => 50,
       :rebounds => 10,
       :assists => 5,
