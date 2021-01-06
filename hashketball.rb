3c09bc0494a5eeea4b64d28f23a941668f1daaa2
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-06 16:31:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,19 +52,19 @@ def game_hash
     ]
   }, 
   :away => {
-    # :team_name => " ",
-    # :colors => [" ", " "],
-    # :players => [
-    # {:player_name => " ", 
-    #   :number => , 
-    #   :shoe => , 
-    #   :points => ,
-    #   :rebounds => ,
-    #   :assists => ,
-    #   :steals => ,
-    #   :blocks => ,
-    #   :slam_dunks => }
-    # ]
+    :team_name => " ",
+    :colors => [" ", " "],
+    :players => [
+    {:player_name => " ", 
+      :number => , 
+      :shoe => , 
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks => }
+    ]
   }
   }
 end
