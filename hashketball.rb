0d624fd16732288bd10c51bde02e1062f1279e79
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-06 14:53:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,33 @@
 def game_hash
-  teams = {:home => {}, :away => {}
+  teams = {
+    :home => {
+    :team_name => " ",
+    :colors => [" ", " "],
+    :players => [{
+      :player_name => " ", 
+      :number => 1, 
+      :shoe => 9, 
+      :points => 50,
+      :rebounds => 10,
+      :assists => 5,
+      :steals => 5,
+      :blocks => 5,
+      :slam_dunks => 5
+    }]
+  }, 
+  :away => {
+    :team_name => " ",
+    :colors => [" ", " "],
+    :players => [{
+      :player_name => " ", 
+      :number => 1, 
+      :shoe => 9, 
+      :points => 50,
+      :rebounds => 10,
+      :assists => 5,
+      :steals => 5,
+      :blocks => 5,
+      :slam_dunks => 5}]
+  }
   }
 end
