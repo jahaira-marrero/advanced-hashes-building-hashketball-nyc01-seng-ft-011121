86d23e7eae0d953f7227a20774af141ba47bc676
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-06 16:31:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -31,7 +31,7 @@ def game_hash
       :steals => 3,
       :blocks => 1,
       :slam_dunks => 15},
-     {:player_name => "Mason Plumlee ", 
+     {:player_name => "Mason Plumlee", 
       :number => 1, 
       :shoe => 19, 
       :points => 26,
